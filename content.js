89947de5d757aca687879078c01901e6cab0d8c0
Kotvič <134196364+KotvicCodes@users.noreply.github.com>
2026-01-09 18:39:11 +0100

    add Tabloid addiction achievemnt
    1.0
    added a way to make Tabloid addiction achievement
    1.1
    added a function called achievement hunt which instantly starts hunting achievements as the runs starts
    1.2
    added 3 s delay upon loading into the page

--- content.js
+++ content.js
@@ -1,14 +1,17 @@
 //
 
 console.log('Alakazam: Content script loaded')
-
 const bigCookieQ = document.getElementById('bigCookie')
 
-setInterval(() => {
-    bigCookieQ?.click()
-    buyUpgrades()
-    buyBakers()
-}, 1)
+setTimeout(() => {
+    setInterval(() => {
+        bigCookieQ?.click()
+        buyUpgrades()
+        buyBakers()
+    }, 1)
+
+    achievementHunt()
+}, 3000)
 
 function buyBakers() {
     const bakers = document.querySelectorAll('.product.unlocked')
@@ -18,4 +21,27 @@ function buyBakers() {
 function buyUpgrades() {
     const upgrades = document.querySelectorAll('.crate.upgrade')
     upgrades.forEach((upgrade) => upgrade.click())
+}
+
+function achievementHunt() {
+    console.log('Achievement hunt started')
+    //* Tabloid addiction
+    const commentsTextQ = document.getElementById('commentsText')
+    const commentsText1Q = document.getElementById('commentsText1')
+    const commentsText2Q = document.getElementById('commentsText2')
+
+    const clickInterval = setInterval(() => {
+        const achievementTitle = document.querySelector('h5 .title')?.innerText
+        const tabloidAddictionAchieved = achievementTitle === 'Tabloid addiction'
+
+        if (tabloidAddictionAchieved) {
+            console.log('Tabloid addiction achieved!')
+            clearInterval(clickInterval)
+        } else {
+            commentsTextQ?.click()
+            commentsText1Q?.click()
+            commentsText2Q?.click()
+            console.log('clicking for tabloid addiction')
+        }
+    }, 100)
 }
