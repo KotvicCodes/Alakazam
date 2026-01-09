6d240feeb5dcd499a1e39c1ecc4d2a641794fbeb
Kotvič <134196364+KotvicCodes@users.noreply.github.com>
2026-01-09 18:05:41 +0100

    auto baker & upgrade buying

--- content.js
+++ content.js
@@ -5,5 +5,17 @@ console.log('Alakazam: Content script loaded')
 const bigCookieQ = document.getElementById('bigCookie')
 
 setInterval(() => {
-     bigCookieQ.click()
+    bigCookieQ?.click()
+    buyUpgrades()
+    buyBakers()
 }, 1)
+
+function buyBakers() {
+    const bakers = document.querySelectorAll('.product.unlocked')
+    bakers.forEach((upgrade) => upgrade.click())
+}
+
+function buyUpgrades() {
+    const upgrades = document.querySelectorAll('.crate.upgrade')
+    upgrades.forEach((upgrade) => upgrade.click())
+}
